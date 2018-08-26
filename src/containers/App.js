fcbbe1a297d2e2a7ea183a40fec166f7363aa54d
sam <sam@svers-MBP.fios-router.home>
2018-08-26 15:54:12 -0400

    working app with redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,38 +1,47 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
+import { setSearchField, requestRobots } from '../actions';
+
+//what piece of state I need to listen to and send it on as props
+const mapStateToProps = state => {
+  return{
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
+  }
+}
+
+//what props I need to listen to that are actions that need to get dispatched
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+  }
+}
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: [],
-      searchField: ''
-    }
-  }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ robots: users }));
+    this.props.onRequestRobots()
   }
-  onSearchChange = (event) => {
-    this.setState({searchField: event.target.value});
-  }
+
   render(){
-    const { robots, searchField } = this.state;
+    const { searchField, onSearchChange, robots, isPending } = this.props;
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
-    return !robots.length?
+    return isPending?
       <h1>Loading</h1>:
       (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
+          <SearchBox searchChange={onSearchChange}/>
           <Scroll>
             <ErrorBoundary>
               <CardList robots={filteredRobots}/>
@@ -42,4 +51,4 @@ class App extends Component {
       );
   }
 }
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
